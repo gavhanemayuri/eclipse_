5d540ff132a10f2281adf84adbd6d394da5fb6fc
Mayuri Gavhane <mayurigavhane1504@gmail.com>
2026-04-04 22:53:48 +0530

    Add files via upload

---
+++ MondayHQL/src/main/java/MondayHQL/MondayHQL/App.java
@@ -0,0 +1,13 @@
+package MondayHQL.MondayHQL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
